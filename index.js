778e6a9c0f81f4c8138fb8b68b6dc85d0a917814
lindyem <lindy.merwin@gmail.com>
2020-06-30 17:35:34 -0700

    Questions array

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 var inquirer = require("inquirer");
 var fs = require("fs");
+
 // array of questions for user
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "Enter the title of your project.",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Enter a description of your project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Enter installation instructions.",
+    name: "instructions",
+  },
+  {
+    type: "input",
+    message: "Enter usage information.",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Enter the contribution guidelines.",
+    name: "contribution",
+  },
+  {
+    type: "input",
+    message: "Enter test instructions.",
+    name: "test",
+  },
+];
 
 // function to write README file
 function writeToFile(fileName, data) {}
